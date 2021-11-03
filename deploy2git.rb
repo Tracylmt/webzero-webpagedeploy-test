fe0db24844730012ca28d9bffdfdbef76de4a8ec
Tracylmt <ml4683@columbia.edu>
2021-11-03 01:55:23 -0400

    uploaded docs

--- deploy2git.rb
+++ deploy2git.rb
@@ -5,7 +5,7 @@ puts Dir.pwd
 puts `cd #{Dir.pwd}/output_local/`
 # puts `bundle add jekyll`
 # puts `bundle add webrick --1.7`
-# puts `git checkout gh-pages`
+puts `git branch -M main`
 puts `bundle exec jekyll build output_local`
 
 # user the command below to test on localhost
